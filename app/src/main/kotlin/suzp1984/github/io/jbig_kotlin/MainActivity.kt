553bdc657b210a29c917cf0d2918804c3555cec7
suzp1984 <suzp1984@gmail.com>
2016-11-10 22:21:47 +0800

    init repo

---
+++ app/src/main/kotlin/suzp1984/github/io/jbig_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package suzp1984.github.io.jbig_kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    protected fun onCreate(savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
