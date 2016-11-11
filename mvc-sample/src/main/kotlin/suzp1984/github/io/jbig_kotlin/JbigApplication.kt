d150d0aba784c9caca62c655cb33aab6af5772f9
suzp1984 <suzp1984@gmail.com>
2016-11-11 16:35:11 +0800

    add butterKnife libary

---
+++ mvc-sample/src/main/kotlin/suzp1984/github/io/jbig_kotlin/JbigApplication.kt
@@ -0,0 +1,14 @@
+package suzp1984.github.io.jbig_kotlin
+
+import android.app.Application
+
+/**
+ * Created by suzhenxi on 11/11/2016.
+ */
+
+class JbigApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- mvc-sample/src/main/kotlin/suzp1984/github/io/jbig_kotlin/MainActivity.kt
+++ mvc-sample/src/main/kotlin/suzp1984/github/io/jbig_kotlin/MainActivity.kt
@@ -2,11 +2,21 @@ package suzp1984.github.io.jbig_kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
 
 class MainActivity : AppCompatActivity() {
+
+    @BindView(R.id.hello_world)
+    lateinit var helloWorld : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ButterKnife.bind(this)
+
+        helloWorld.text = "Hello Again !!"
     }
 }
