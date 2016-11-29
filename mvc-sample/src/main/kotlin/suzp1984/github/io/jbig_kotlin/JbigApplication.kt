9cd8390c1fba38b5afa44d8f2aa67f4d51a12939
suzp1984 <suzp1984@gmail.com>
2016-11-29 16:52:21 +0800

    upgrade dagger; open StrictMode

--- mvc-sample/src/main/kotlin/suzp1984/github/io/jbig_kotlin/JbigApplication.kt
+++ mvc-sample/src/main/kotlin/suzp1984/github/io/jbig_kotlin/JbigApplication.kt
@@ -1,6 +1,8 @@
 package suzp1984.github.io.jbig_kotlin
 
 import android.app.Application
+import android.os.StrictMode
+import butterknife.ButterKnife
 
 /**
  * Created by moses on 11/11/2016.
@@ -9,6 +11,21 @@ import android.app.Application
 class JbigApplication : Application() {
 
     override fun onCreate() {
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build())
+
+            StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build())
+
+            ButterKnife.setDebug(true)
+        }
+
         super.onCreate()
     }
 }
